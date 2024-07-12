05ffa07c64bf62da1f6ee7ac0c319b1876a775a6
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-07-12 10:00:30 -0300

    feature/add-header/footer/routes

---
+++ src/pages/PixPage/index.tsx
@@ -0,0 +1,9 @@
+import { PixPageContainer } from "./styles"
+
+export const PixPage = () => {
+    return(
+        <PixPageContainer>
+           <h1>PixPage</h1>
+        </PixPageContainer>
+    )
+}
