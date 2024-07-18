7a1732da78b3c6f17be03de2b37e3fb4212486fa
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-07-17 23:56:49 -0300

    feature/update-pixPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
-import { PaymentMetod } from "./pages/PaymentMetodPage";
+// import { PaymentMetod } from "./pages/PaymentMetodPage";
 // import { PixInInstallmentsPage } from "./pages/PixInInstallmentsPage";
-// import { PixPage } from "./pages/PixPage";
+import { PixPage } from "./pages/PixPage";
 
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<PaymentMetod />} />
+        {/* <Route path="/" element={<PaymentMetod />} /> */}
         {/* <Route path="/" element={<PixInInstallmentsPage />} /> */}
-        {/* <Route path="/" element={<PixPage/>} /> */}
+        <Route path="/" element={<PixPage/>} />
       </Route>
     </Routes>
   );
