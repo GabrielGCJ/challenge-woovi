fea8677abec5df62fa7dc0b00e9b02c01f229b63
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-07-20 14:09:43 -0300

    feature/new_updates

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,4 +4,6 @@ export const FooterContainer = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  padding: 3rem 0 5rem 0;
 `;
--- src/pages/PixPage/styles.ts
+++ src/pages/PixPage/styles.ts
@@ -6,12 +6,17 @@ export const PixPageContainer = styled.div`
   justify-content: center;
   flex-direction: column;
   gap: 1rem;
-  padding-bottom: 5rem;
+
   font-family: "Nunito", sans-serif;
 
   h2 {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
     font-size: 24px;
     color: #4d4d4d;
+    max-width: 300px;
   }
 
   .qr {
@@ -28,14 +33,14 @@ export const PixPageContainer = styled.div`
     border: none;
     border-radius: 10px;
     font-family: "Nunito", sans-serif;
-    font-size: 20px;
-    color: #4d4d4d;
+    font-size: 18px;
+    color: #FFFFFF;
     min-width: 14 2563225rem;
+    padding: 0.5rem 1.5rem;
     /* min-width: 200px ; */
 
     .icon {
       width: 1.5rem;
-
       padding-left: 0.5rem;
     }
   }
@@ -50,23 +55,62 @@ export const TermTime = styled.div`
   padding: 0;
 
   .term {
-    color: rgb(117, 117, 117);
+    color: #b2b2b2;
   }
   .date {
-    color: white;
+    color: #4d4d4d;
   }
 `;
 
 export const InstallmentsContainer = styled.div`
-  border-bottom: 2px solid black;
+  border-bottom: 2px solid rgb(229, 229, 229);
+  padding-bottom: 1rem;
 `;
 
 export const Installment = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
   width: 100%;
   max-width: 30rem;
   min-width: 20rem;
+`;
+
+export const TotalPayment = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 30rem;
+  min-width: 20rem;
+  border-bottom: 2px solid rgb(229, 229, 229);
+  padding-bottom: 1rem;
+`;
+
+export const MoreOptions = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 30rem;
+  min-width: 20rem;
+  border-bottom: 2px solid rgb(229, 229, 229);
+  padding-bottom: 1rem;
+`;
+
+export const IdentifierContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  max-width: 30rem;
+  min-width: 20rem;
+  font-size:14px ;
+
+  .identifier {
+    color: rgb(178, 178, 178);
+  }
+
+  .identifierNum {
+    color: rgb(77, 77, 77);
+  }
 `;
