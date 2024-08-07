70c19cb08580fd3b70d8146b2fa1891bd3698ded
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-08-07 13:56:10 -0300

    feature/update-ParcelInformation

--- src/components/ParcelInformation/index.tsx
+++ src/components/ParcelInformation/index.tsx
@@ -1,40 +1,12 @@
-import {
-  FirstValueContainer,
-  FirstValueContainerGlobal,
-  LeftContainer,
-  ParcelInformationContainer,
-  RigthContainer,
-  SecondaryValueContainer,
-} from "./styles";
+import { FirstValueContainerGlobal } from "./styles";
 
-import ima1 from "../../assets/elipse1.svg";
-import ima2 from "../../assets/ellipse2.svg";
+// import ima1 from "../../assets/elipse1.svg";
+// import ima2 from "../../assets/ellipse2.svg";
 
 export const ParcelInformation = () => {
   return (
     <FirstValueContainerGlobal>
-
-    <ParcelInformationContainer>
-      <FirstValueContainer>
-        <LeftContainer>
-          <img src={ima1} alt="" />
-          <h3>1ª entrada no Pix</h3>
-        </LeftContainer>
-        <RigthContainer>
-          <h3>R$ 15.300,00</h3>
-        </RigthContainer>
-      </FirstValueContainer>
-
-      <SecondaryValueContainer>
-        <LeftContainer>
-          <img src={ima2} alt="" />
-          <h3>2ª no cartão</h3>
-        </LeftContainer>
-        <RigthContainer>
-          <h3>R$ 15.300,00</h3>
-        </RigthContainer>
-      </SecondaryValueContainer>
-    </ParcelInformationContainer>
+      <h1>Continue</h1>
     </FirstValueContainerGlobal>
   );
 };
